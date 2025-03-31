40ba00effb1c0ff231febc5ea5871a349231c114
ruudiac <133976166+ruudiac@users.noreply.github.com>
2025-03-31 15:04:09 +0300

    Add files via upload

---
+++ EngestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngestApp.swift
+//  Engest
+//
+//  Created by Gertrud Roos on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct EngestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
